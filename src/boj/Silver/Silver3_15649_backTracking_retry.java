aedf94d3f082efecfdd1f6a3ea8410e2d49aa0c7
guntaek <rjsxor7584@naver.com>
2025-09-01 23:51:32 +0900

    백준 실버3 15649 N과 M(1)

---
+++ src/boj/Silver/Silver3_15649_backTracking_retry.java
@@ -0,0 +1,44 @@
+package boj.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Silver3_15649_backTracking_retry {
+    static int N, M;
+    static int[] pick;
+    static boolean[] used;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        pick = new int[M];
+        used = new boolean[N + 1];
+
+        dfs(0);
+        System.out.print(sb);
+    }
+
+    static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(pick[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int num = 1; num <= N; num++) {
+            if (used[num]) continue;
+            used[num] = true;
+            pick[depth] = num;
+            dfs(depth + 1);
+            used[num] = false;
+        }
+    }
+}
