1b7926755a3e9a40fe76a704179127967e0f29e5
guntaek <rjsxor7584@naver.com>
2025-09-13 23:03:50 +0900

    백준 실버2 15663, 15665, 15666 N과 M

--- src/Test2.java
+++ src/Test2.java
@@ -1,10 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Test2 {
     public static void main(String[] args) throws IOException {
+        solution("KAKAO");
+    }
 
+    static int[] solution(String msg) {
+        List<Integer> answer = new ArrayList<>();
+        Map<String, Integer> dict = new HashMap<>();
+
+        // 1. 사전 초기화 (A-Z를 1-26번으로)
+        for (int i = 0; i < 26; i++) {
+            dict.put(String.valueOf((char)('A' + i)), i + 1);
+        }
+
+        int dictIdx = 27; // 다음 사전 색인 번호
+        int idx = 0; // 현재 문자열 인덱스
+
+        while (idx < msg.length()) {
+            String w = "";
+            String c = "";
+
+            // 2. 사전에서 현재 입력과 일치하는 가장 긴 문자열 w 찾기
+            int j = idx;
+            while (j < msg.length()) {
+                String temp = msg.substring(idx, j + 1);
+                if (dict.containsKey(temp)) {
+                    w = temp;
+                    j++;
+                } else {
+                    c = msg.substring(j, j + 1);
+                    break;
+                }
+            }
+
+            // 3. w에 해당하는 색인 번호 출력
+            answer.add(dict.get(w));
+
+            // 4. 입력에서 처리되지 않은 다음 글자가 있다면 w+c를 사전에 등록
+            if (!c.isEmpty()) {
+                dict.put(w + c, dictIdx++);
+            }
+
+            // 5. 입력에서 w 제거
+            idx += w.length();
+        }
+
+        // List를 배열로 변환
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
---
+++ src/boj/Silver/Silver2_15665.java
@@ -0,0 +1,55 @@
+package boj.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Silver2_15665 {
+    static int N, M;
+    static int[] src;
+    static int[] pick;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        src = new int[N];
+        pick = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            src[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(src);
+
+        dfs(0);
+
+        System.out.println(sb);
+
+    }
+
+    static void dfs(int depth) {
+        if (depth == M) {
+            for (int num : pick) {
+                sb.append(num).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        int prev = -1;
+
+        for (int i = 0; i < N; i++) {
+            if (src[i] == prev) continue;
+            pick[depth] = src[i];
+            prev = src[i];
+            dfs(depth + 1);
+        }
+    }
+}
