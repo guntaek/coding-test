19156258b8a5580d3b62fd512758e41ce63fb640
guntaek <rjsxor7584@naver.com>
2025-09-15 23:17:14 +0900

    백준 실버2 15666 N과 M(12)

--- src/boj/Silver/Silver2_15666.java
+++ src/boj/Silver/Silver2_15666.java
@@ -8,9 +8,10 @@ import java.util.StringTokenizer;
 
 public class Silver2_15666 {
     static int N, M;
-    static int[] src;
-    static int[] pick;
+    static int[] src;      // 입력받은 숫자들
+    static int[] pick;      // 선택한 M개의 숫자들
     static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -28,28 +29,28 @@ public class Silver2_15666 {
 
         Arrays.sort(src);
 
-        dfs(0);
-
-        System.out.println(sb);
-
+        dfs(0, 0);
+        System.out.print(sb);
     }
 
-    static void dfs(int depth) {
+    static void dfs(int depth, int start) {
         if (depth == M) {
-            for (int num : pick) {
-                sb.append(num).append(' ');
+            for (int i = 0; i < M; i++) {
+                sb.append(pick[i]).append(" ");
             }
-            sb.append('\n');
+            sb.append("\n");
             return;
         }
 
-        int prev = -1;
+        int prev = 0;
 
-        for (int i = 0; i < N; i++) {
-            if (src[i] == prev) continue;
+        for (int i = start; i < N; i++) {
+            if (prev == src[i]) continue;
+
             pick[depth] = src[i];
             prev = src[i];
-            dfs(depth + 1);
+
+            dfs(depth + 1, i);
         }
     }
 }
